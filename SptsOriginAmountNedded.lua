d829bdc2c3f18014af7dd81ca3f1f3242f28f3fb
Lost <93594637+ItzLost@users.noreply.github.com>
2023-02-25 17:02:57 +0100

    Update SptsOriginAmountNedded.lua

--- SptsOriginAmountNedded.lua
+++ SptsOriginAmountNedded.lua
@@ -5,12 +5,12 @@ local AmountNeed = {
     [2] = 1e12, -- 1T
     [3] = 1e9, -- 1B
     [4] = 1e6, -- 1M
-  }
+  },
   Fist = {
     [1] = 10e12, -- 10T
     [2] = 100e9, -- 100B
     [3] = 1e9, -- 1B 
-  }
+  },
 }
 
 return(AmountNeed)
